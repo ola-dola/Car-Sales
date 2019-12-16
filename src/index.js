079fd2d1ef7da9493bbded0ee903dcd591cac192
oredola <oredolaolamide@gmail.com>
2019-12-16 09:14:29 +0100

    Setup redux provider for the app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
+import { featuresReducer } from './state/reducers';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const monsterReducer = combineReducers({
+  features: featuresReducer,
+})
+
+const store = createStore(
+  monsterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
