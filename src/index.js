28b2386e4973049acb9f8fc752b15861d343f216
ola-dola <oredolaolamide@gmail.com>
2020-06-20 23:19:36 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { combineReducers, createStore } from "redux";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import reducer from "./state/reducer";
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+// const monsterReducer = combineReducers()
+const store = createStore(reducer);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
